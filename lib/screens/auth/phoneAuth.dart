89195f330e674169a411dadf80a55c58d1838261
AlphaArtrem <yashawasthi01@gmail.com>
2021-02-18 14:21:32 +0530

    Phone auth screens

--- lib/screens/auth/phoneAuth.dart
+++ lib/screens/auth/phoneAuth.dart
@@ -1,4 +1,9 @@
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:ecom_app_flutter/common/sizing.dart';
+import 'package:ecom_app_flutter/common/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class PhoneAuth extends StatefulWidget {
   @override
@@ -6,9 +11,382 @@ class PhoneAuth extends StatefulWidget {
 }
 
 class _PhoneAuthState extends State<PhoneAuth> {
+  int _phoneNumber = 9415452686;
+  CountryCode _code;
+  bool _enterNumberState = true;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _enterNumberState ? _enterNumberForm() : _enterOTPState(),
+    );
+  }
+
+  Widget _enterNumberForm(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+            left: Sizing.getProportionateScreenWidth(20),
+            top: Sizing.getProportionateScreenWidth(20),
+          ),
+          height: Sizing.screenHeight * 0.22,
+          width: Sizing.screenWidth,
+          decoration: BoxDecoration(
+              gradient: kPrimaryGradientColor,
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(170)
+              )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "What Is Your Phone",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: Sizing.getProportionateScreenWidth(25),
+                    fontWeight: FontWeight.w500,
+                    decoration: TextDecoration.none,
+                    fontFamily: "arial"
+                ),
+              ),
+              Text(
+                "Number?",
+                style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: Sizing.getProportionateScreenWidth(25),
+                    fontWeight: FontWeight.w500,
+                    decoration: TextDecoration.none,
+                    fontFamily: "arial"
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: Sizing.getProportionateScreenHeight(20),
+        ),
+        Expanded(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(Sizing.getProportionateScreenWidth(20)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Please enter your phone number to verify your account",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: Sizing.getProportionateScreenWidth(15),
+                        fontWeight: FontWeight.w300,
+                        decoration: TextDecoration.none,
+                        fontFamily: "arial"
+                    ),
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(Sizing.getProportionateScreenWidth(5.0)),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        border: Border.all(
+                            color: Colors.grey.shade300,
+                            width: Sizing.getProportionateScreenWidth(0.5
+                            )
+                        )
+                    ),
+                    child: Row(
+                      children: [
+                        CountryCodePicker(
+                          onChanged: (code){
+                            _code = code;
+                          },
+                          onInit: (code){
+                            _code = code;
+                          },
+                          dialogTextStyle: TextStyle(
+                              color: Colors.black45
+                          ),
+                          initialSelection: 'IN',
+                          favorite: ['+91','IN'],
+                          showCountryOnly: false,
+                          showOnlyCountryWhenClosed: false,
+                          alignLeft: false,
+                        ),
+                        SizedBox(width: Sizing.getProportionateScreenWidth(5),),
+                        Expanded(
+                          child: TextFormField(
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: _phoneNumber.toString(),
+                                hintStyle: TextStyle(color: Colors.grey)
+                            ),
+                            initialValue: "",
+                            keyboardType: TextInputType.phone,
+                            onChanged: (val){
+                              _phoneNumber = int.parse(val);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  MaterialButton(
+                    elevation: 0.0,
+                    minWidth: Sizing.screenWidth * 0.9,
+                    height: Sizing.getProportionateScreenHeight(50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    onPressed: (){
+                      setState(() {
+                        _enterNumberState = false;
+                      });
+                    },
+                    color: Color.fromRGBO(238, 213, 26, 1),
+                    textColor: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Sizing.getProportionateScreenWidth(40),
+                        vertical: Sizing.getProportionateScreenHeight(10),
+                      ),
+                      child: Text(
+                        "Send Verification Code",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizing.getProportionateScreenWidth(15),
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: Text(
+                      "Skip",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: Sizing.getProportionateScreenWidth(15),
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.none,
+                          fontFamily: "arial"
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _enterOTPState(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+            left: Sizing.getProportionateScreenWidth(20),
+            top: Sizing.getProportionateScreenWidth(20),
+          ),
+          height: Sizing.screenHeight * 0.22,
+          width: Sizing.screenWidth,
+          decoration: BoxDecoration(
+              gradient: kPrimaryGradientColor,
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(170)
+              )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Verification",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: Sizing.getProportionateScreenWidth(25),
+                    fontWeight: FontWeight.w500,
+                    decoration: TextDecoration.none,
+                    fontFamily: "arial"
+                ),
+              ),
+              Text(
+                "Code",
+                style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: Sizing.getProportionateScreenWidth(25),
+                    fontWeight: FontWeight.w500,
+                    decoration: TextDecoration.none,
+                    fontFamily: "arial"
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: Sizing.getProportionateScreenHeight(20),
+        ),
+        Expanded(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(Sizing.getProportionateScreenWidth(20)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Please enter code sent to",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: Sizing.getProportionateScreenWidth(15),
+                            fontWeight: FontWeight.w300,
+                            decoration: TextDecoration.none,
+                            fontFamily: "arial"
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "${_code.dialCode} $_phoneNumber",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: Sizing.getProportionateScreenWidth(15),
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.none,
+                            fontFamily: "arial"
+                        ),
+                      ),
+                      Spacer(),
+                      InkWell(
+                        onTap: (){
+                          setState(() {
+                            _enterNumberState = true;
+                          });
+                        },
+                        child: Text(
+                          "Change Phone Number",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: Sizing.getProportionateScreenWidth(12),
+                              fontWeight: FontWeight.w500,
+                              decoration: TextDecoration.underline,
+                              fontFamily: "arial"
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: Sizing.getProportionateScreenWidth(25)
+                    ),
+                    child: PinCodeTextField(
+                      appContext: context,
+                      length: 4,
+                      obscureText: false,
+                      animationType: AnimationType.fade,
+                      keyboardType: TextInputType.number,
+                      pinTheme: PinTheme(
+                        inactiveFillColor: Colors.white,
+                        selectedFillColor: Colors.white,
+                        shape: PinCodeFieldShape.underline,
+                        borderRadius: BorderRadius.circular(5),
+                        fieldHeight: Sizing.getProportionateScreenHeight(40),
+                        fieldWidth: Sizing.getProportionateScreenWidth(40),
+                        activeFillColor: Colors.white,
+                      ),
+                      animationDuration: Duration(milliseconds: 300),
+                      enableActiveFill: true,
+                      onCompleted: (v) {
+                      },
+                      onChanged: (value) {},
+                      beforeTextPaste: (text) {
+                        return true;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  MaterialButton(
+                    minWidth: Sizing.screenWidth * 0.9,
+                    height: Sizing.getProportionateScreenHeight(50),
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    onPressed: (){
+                      Navigator.pushReplacement(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => PhoneAuth()
+                          )
+                      );
+                    },
+                    color: Color.fromRGBO(238, 213, 26, 1),
+                    textColor: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Sizing.getProportionateScreenWidth(40),
+                        vertical: Sizing.getProportionateScreenHeight(10),
+                      ),
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizing.getProportionateScreenWidth(15),
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  InkWell(
+                    onTap: (){},
+                    child: Text(
+                      "Resend Code",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: Sizing.getProportionateScreenWidth(15),
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.none,
+                          fontFamily: "arial"
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,6 +1,7 @@
 import 'package:ecom_app_flutter/common/sizing.dart';
 import 'package:ecom_app_flutter/common/styles.dart';
 import 'package:ecom_app_flutter/screens/auth/log_in.dart';
+import 'package:ecom_app_flutter/screens/auth/phoneAuth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -16,120 +17,125 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     Sizing().init(context);
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              height: Sizing.screenHeight,
-              width: Sizing.screenWidth,
-              child: Image.asset(
-                "assets/splash.jpg",
-                fit: BoxFit.cover,
-              ),
+      body: Stack(
+        children: [
+          Container(
+            height: Sizing.screenHeight,
+            width: Sizing.screenWidth,
+            child: Image.asset(
+              "assets/splash.jpg",
+              fit: BoxFit.cover,
             ),
-            Container(
-              height: Sizing.screenHeight,
-              width: Sizing.screenWidth,
-              color: kPrimaryColor.withOpacity(0.25),
+          ),
+          Container(
+            height: Sizing.screenHeight,
+            width: Sizing.screenWidth,
+            color: kPrimaryColor.withOpacity(0.25),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: Sizing.screenHeight * 0.575,
+              left: Sizing.screenWidth * 0.35
             ),
-            Container(
-              margin: EdgeInsets.only(
-                top: Sizing.screenHeight * 0.575,
-                left: Sizing.screenWidth * 0.35
-              ),
-              decoration: BoxDecoration(
-                color: Colors.yellow.withOpacity(0.5),
+            decoration: BoxDecoration(
+              color: Colors.yellow.withOpacity(0.5),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(100)
+              )
+            ),
+            child: Container(),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: Sizing.screenHeight * 0.65,
+            ),
+            decoration: BoxDecoration(
+                gradient: kPrimaryGradientColor,
                 borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(100)
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)
                 )
-              ),
-              child: Container(),
             ),
-            Container(
-              margin: EdgeInsets.only(
-                  top: Sizing.screenHeight * 0.65,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Sizing.getProportionateScreenWidth(50),
+                vertical: Sizing.getProportionateScreenHeight(30)
               ),
-              decoration: BoxDecoration(
-                  gradient: kPrimaryGradientColor,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40)
-                  )
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizing.getProportionateScreenWidth(50),
-                  vertical: Sizing.getProportionateScreenHeight(30)
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Quickie",
-                          style: TextStyle(
-                            color: kTextColor,
-                            fontSize: Sizing.getProportionateScreenWidth(25),
-                            fontWeight: FontWeight.bold
-                          ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Quickie",
+                        style: TextStyle(
+                          color: kTextColor,
+                          fontSize: Sizing.getProportionateScreenWidth(25),
+                          fontWeight: FontWeight.bold
                         ),
-                        Text(
-                          "Shop",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: Sizing.getProportionateScreenWidth(25),
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: Sizing.getProportionateScreenHeight(20),
-                    ),
-                    Flexible(
-                      child: Text(
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                      ),
+                      Text(
+                        "Shop",
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: Sizing.getProportionateScreenWidth(14),
-                          fontWeight: FontWeight.w400
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    SizedBox(
-                      height: Sizing.getProportionateScreenHeight(40),
-                    ),
-                    MaterialButton(
-                      elevation: 0.0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      onPressed: (){},
-                      color: Colors.yellow,
-                      textColor: Colors.white,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                         horizontal: Sizing.getProportionateScreenWidth(40)
-                        ),
-                        child: Text(
-                          "Get Started",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: Sizing.getProportionateScreenWidth(15),
-                              fontWeight: FontWeight.bold
-                          ),
+                          fontSize: Sizing.getProportionateScreenWidth(25),
+                          fontWeight: FontWeight.bold
                         ),
                       ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(20),
+                  ),
+                  Flexible(
+                    child: Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizing.getProportionateScreenWidth(14),
+                        fontWeight: FontWeight.w400
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: Sizing.getProportionateScreenHeight(40),
+                  ),
+                  MaterialButton(
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    onPressed: (){
+                      Navigator.pushReplacement(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => PhoneAuth()
+                          )
+                      );
+                    },
+                    color: Color.fromRGBO(238, 213, 26, 1),
+                    textColor: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                       horizontal: Sizing.getProportionateScreenWidth(40)
+                      ),
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizing.getProportionateScreenWidth(15),
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
